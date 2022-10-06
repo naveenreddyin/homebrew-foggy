e0a0e0b15d36b198e757951d8accab935b12d7bf
VASUDEVAN Naveen (EXT-RIC-CH) <naveen.vasudevan@VASUDEVANs-MacBook-Pro.local>
2022-10-06 17:00:40 +0200

    refactor: sha valid put

--- Formula/foggy.rb
+++ Formula/foggy.rb
@@ -2,7 +2,7 @@ class Foggy < Formula
   desc "CLI for getting random dog facts, thus Foggy!"
   homepage "https://github.com/naveenreddyin/randog-cli.git"
   url "https://github.com/naveenreddyin/randog-cli/releases/download/v0.2.0/randog-cli-0.2.0-x86_64-apple-darwin.tar.gz"
-  sha256 "a373841e34cce377e3d33862295d3d589c2b627e"
+  sha256 "194b786af87e80d67d14faf9dfc754918170cdbf7a6aa033fa869760421aa3f3"
   version "0.2.0"
 
   def install
